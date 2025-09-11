34b60f22d14bcc75445fadb377dd218256eed57c
choiminji <chlals9930@gmail.com>
2025-09-11 15:11:16 +0900

    Topics 상수 변경

--- src/main/java/kr/hhplus/be/server/common/constant/Topics.java
+++ src/main/java/kr/hhplus/be/server/common/constant/Topics.java
@@ -1,6 +1,11 @@
 package kr.hhplus.be.server.common.constant;
 
 public class Topics {
+    // Topic
     public static final String ORDER_COMPLETED = "order-completed";
     public static final String COUPON_ISSUE_REQUEST = "coupon-issue-request";
+    
+    // Consumer
+    public static final String ORDER_DATA_GROUP = "order-data-group";
+    public static final String COUPON_ISSUE_GROUP = "coupon-issue-group";
 }
